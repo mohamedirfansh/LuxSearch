4ce8d7fa3ea6ebb8cd23228d1a631cbf784059ad
Mohamed Irfan <irfancuber@gmail.com>
2023-02-15 01:15:49 +0800

    Configure backend with CORS and register blueprints

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from flask_restful import Api
+from flask_cors import CORS, cross_origin
+from endpoints.search import search
 
+# init Flask app
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello():
-    return 'hi'
+# register blueprints
+#app.register_blueprint(search, url_prefix='/api/search/<int:id>')
+app.register_blueprint(search, url_prefix='/api/search')
 
 if __name__ == '__main__':
     app.run(debug=True)
