cfebf830b8326d56a35bfea6495cd5826d053a9e
Mohamed Irfan <irfancuber@gmail.com>
2023-03-23 20:10:39 +0800

    Add endpoint to query reddit posts in backend

---
+++ backend/endpoints/reddit.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify, request
+from dummy.data import dummyResponse
+from config import USE_MOCK_DATA
+from utils.elasticsearch import elastic_client
+
+reddit = Blueprint(name='reddit', import_name=__name__)
+
+@reddit.route('/', methods=['GET'])
+def reddit_search():
+    if USE_MOCK_DATA:
+        output = dummyResponse
+        return jsonify(output)
+    
+    query = request.args.get('q')
+    query_body = {
+        "size": 10,
+        "query": {
+            "match": {
+            "original_post": query
+            }
+        }
+    }
+
+    res = elastic_client.search(index="reddit", body=query_body)
+    return jsonify(res)
