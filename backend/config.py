a4965cbf753cd1dad9d5bbc9f881ca00253ae907
Mohamed Irfan <irfancuber@gmail.com>
2023-03-30 00:42:55 +0800

    Add custom ranking score using likes, retweets, upvotes & comments

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,9 @@
 USE_MOCK_DATA = False
+RANK_SCORE = {
+    # Twitter
+    "likes": 10000,
+    "retweets": 1.1,
+    # Reddit
+    "upvotes": 1,
+    "comments": 1
+}
--- backend/endpoints/search.py
+++ backend/endpoints/search.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from dummy.data import dummyResponse
-from config import USE_MOCK_DATA
+from config import USE_MOCK_DATA, RANK_SCORE
 from utils.elasticsearch import elastic_client
 
 search = Blueprint(name='search', import_name=__name__)
@@ -12,14 +12,59 @@ def search_query():
         return jsonify(output)
 
     query = request.args.get('q')
+    start = request.args.get('start', 0)
     query_body = {
+        "from": start,
         "size": 10,
         "query": {
-            "match": {
-            "processed_tweet": query
+            "function_score": {
+                "query": {
+                    "multi_match": {
+                        "query": query,
+                        "fields": ["processed_tweet", "original_post"],
+                        "type": "most_fields",
+                    }
+                },
+                "functions": [
+                    {
+                        "field_value_factor": {
+                            "field": "likes",
+                            "factor": RANK_SCORE['likes'],
+                            "missing": 0,
+                        },
+                        "filter": {"term": {"_index": "twitter"}},
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "retweets",
+                            "factor": RANK_SCORE['retweets'],
+                            "missing": 0,
+                        },
+                        "filter": {"term": {"_index": "twitter"}},
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "upvotes",
+                            "factor": RANK_SCORE['upvotes'],
+                            "missing": 0,
+                        },
+                        "filter": {"term": {"_index": "reddit"}},
+                    },
+                    {
+                        "field_value_factor": {
+                            "field": "comments",
+                            "factor": RANK_SCORE['comments'],
+                            "missing": 0,
+                        },
+                        "filter": {"term": {"_index": "reddit"}},
+                    },
+                ],
+                "score_mode": "sum",
+                "boost_mode": "sum",
             }
-        }
+        },
     }
 
-    res = elastic_client.search(index="twitter", body=query_body)
+
+    res = elastic_client.search(index="twitter,reddit", body=query_body)
     return jsonify(res)
