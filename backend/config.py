ad1ee9a69a69deb3358837c2cf22571bfed1fc1c
Mohamed Irfan <irfancuber@gmail.com>
2023-04-08 20:44:10 +0800

    Modify ranking score weights

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,9 @@
 USE_MOCK_DATA = False
 RANK_SCORE = {
     # Twitter
-    "likes": 0.1,
+    "likes": 0.2,
     "retweets": 0.1,
     # Reddit
     "upvotes": 0.5,
-    "comments": 0.5
+    "comments": 0.4
 }
